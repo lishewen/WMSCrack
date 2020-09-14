0d932c3dc7cc8a22e7191eb4f7f8ec7a629e8704
黎摄文 <lishewen@gmail.com>
2020-09-15 05:57:48 +0800

    添加项目文件。

---
+++ WMSCrack/AESZF2006.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WMSCrack
+{
+   public class AESZF2006
+    {
+		public AESZF2006()
+		{
+			mobjCryptoService = new RijndaelManaged();
+		}
+
+		public void AESKeySet(string sValue)
+		{
+			this.AESKey = sValue;
+		}
+
+		public void AESKeyIVSet(string sValue)
+		{
+			this.AESKeyIV = sValue;
+		}
+
+		private byte[] GetLegalKey()
+		{
+			string text = this.AESKey;
+			this.mobjCryptoService.GenerateKey();
+			byte[] key = this.mobjCryptoService.Key;
+			int num = key.Length;
+			if (text.Length > num)
+			{
+				text = text.Substring(0, num);
+			}
+			else if (text.Length < num)
+			{
+				text = text.PadRight(num, ' ');
+			}
+			return Encoding.ASCII.GetBytes(text);
+		}
+
+		private byte[] GetLegalIV()
+		{
+			string text = this.AESKeyIV;
+			this.mobjCryptoService.GenerateIV();
+			byte[] iv = this.mobjCryptoService.IV;
+			int num = iv.Length;
+			if (text.Length > num)
+			{
+				text = text.Substring(0, num);
+			}
+			else if (text.Length < num)
+			{
+				text = text.PadRight(num, ' ');
+			}
+			return Encoding.ASCII.GetBytes(text);
+		}
+
+		public string AESEncrypto(string Source)
+		{
+			byte[] bytes = Encoding.UTF8.GetBytes(Source + AESZF2006.strAES);
+			MemoryStream memoryStream = new MemoryStream();
+			this.mobjCryptoService.Key = this.GetLegalKey();
+			this.mobjCryptoService.IV = this.GetLegalIV();
+			ICryptoTransform transform = this.mobjCryptoService.CreateEncryptor();
+			CryptoStream cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+			cryptoStream.Write(bytes, 0, bytes.Length);
+			cryptoStream.FlushFinalBlock();
+			memoryStream.Close();
+			byte[] inArray = memoryStream.ToArray();
+			return Convert.ToBase64String(inArray);
+		}
+
+		public string AESDecrypto(string Source)
+		{
+			string result;
+			try
+			{
+				if (Source != "0" && Source != "")
+				{
+					byte[] array = Convert.FromBase64String(Source);
+					MemoryStream stream = new MemoryStream(array, 0, array.Length);
+					this.mobjCryptoService.Key = this.GetLegalKey();
+					this.mobjCryptoService.IV = this.GetLegalIV();
+					ICryptoTransform transform = this.mobjCryptoService.CreateDecryptor();
+					CryptoStream stream2 = new CryptoStream(stream, transform, CryptoStreamMode.Read);
+					StreamReader streamReader = new StreamReader(stream2);
+					result = streamReader.ReadToEnd().Replace(AESZF2006.strAES, "");
+				}
+				else
+				{
+					result = "";
+				}
+			}
+			catch
+			{
+				result = "";
+			}
+			return result;
+		}
+
+		public static string strAES = "lkjlsadf#98234FD)(L:DFS#@T()GDFOKDfsf2r0=3589DFOI#";
+
+		private readonly SymmetricAlgorithm mobjCryptoService;
+
+		private string AESKey = "";
+
+		private string AESKeyIV = "";
+	}
+}
---
+++ WMSCrack/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WMSCrack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly string[] array = new string[]
+        {
+            "5N93e3d03291A9c5072fbcX5285PO316",
+            "c9dGP9da1aXf7a9F79b3a0d50c5aM7c"
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AESZF2006 aeszf = new AESZF2006();
+            aeszf.AESKeySet(array[0]);
+            aeszf.AESKeySet(array[1]);
+            string[] array2 = new string[]
+            {
+                tb_ckx1.Text.Trim(),
+                tb_ckx2.Text.Trim(),
+                tb_ckx3.Text.Trim(),
+                tb_ckx4.Text.Trim()
+            };
+            array2[0] = aeszf.AESDecrypto(array2[0]);
+            array2[1] = aeszf.AESDecrypto(array2[1]);
+            array2[2] = aeszf.AESDecrypto(array2[2]);
+            array2[3] = aeszf.AESDecrypto(array2[3]);
+            tb_rs1.Text = array2[0];
+            tb_rs2.Text = array2[1];
+            tb_rs3.Text = array2[2];
+            tb_rs4.Text = array2[3];
+        }
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AESZF2006 aeszf = new AESZF2006();
+            aeszf.AESKeySet(array[0]);
+            aeszf.AESKeySet(array[1]);
+            string[] array2 = new string[]
+            {
+                tb1_ckx1.Text.Trim(),
+                tb1_ckx2.Text.Trim(),
+                tb1_ckx3.Text.Trim(),
+                tb1_ckx4.Text.Trim()
+            };
+            array2[0] = aeszf.AESEncrypto(array2[0]);
+            array2[1] = aeszf.AESEncrypto(array2[1]);
+            array2[2] = aeszf.AESEncrypto(array2[2]);
+            array2[3] = aeszf.AESEncrypto(array2[3]);
+            tb1_rs1.Text = array2[0];
+            tb1_rs2.Text = array2[1];
+            tb1_rs3.Text = array2[2];
+            tb1_rs4.Text = array2[3];
+        }
+    }
+}
